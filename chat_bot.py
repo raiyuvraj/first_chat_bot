886974fe329aa8b6236a1dc16491b1c545f50935
Your Name <you@example.com>
2024-04-28 22:14:20 +0530

    First Projet

---
+++ chat_bot.py
@@ -0,0 +1,34 @@
+import random
+
+# Dictionary of responses: Fisrst Project
+
+responses = {
+    "hi": ["Hello !!", "Hey !!", "Hi, how can I help you ?"],
+    "how are you ?": ["I am doing well, thank you !!", "I'm good, thank for asking. "],
+    "bye" : ["Good bye !!", "Bye !!", "See you later.", "Have a great day !!"],
+    "default" : ["Sorry, I didn't understand that.", "I'm not sure what do you mean."]
+}
+
+# for generating responses:
+
+def get_responses(message):
+    message= message.lower()
+    if message in responses:
+        return random.choice(responses[message])
+    else:
+        return random.choice(responses["default"])
+
+# for getting user input:
+
+def main():
+    print("Chatbot: Hello, How can i help you ?") 
+    while True:
+        user_input = input("You: ")
+        if user_input.lower()== "exit":
+            print("Chatbot: Good Bye !!")
+            break
+        response = get_responses(user_input)
+        print("Chat Bot: ", response)
+# calling the method
+main()
+  
